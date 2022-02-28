e01047567e73e2fec6e18169b0595e9edb7019ea
PHILLJAY <philip.jasionowski@gmail.com>
2022-02-28 17:56:19 -0500

    initial commit

---
+++ makefile
@@ -0,0 +1,5 @@
+# Philip Jasionowski, 100751888
+#Operating Systems Lab 2
+philshell: philshell.c philshell.h utility.c
+    gcc -Wall philshell.c utility.c -o philshell
+
---
+++ philshell.c
@@ -0,0 +1,29 @@
+int main (int argc, char **argv)
+{
+    //run infinite loop
+    phil_loop();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return EXIT_SUCCESS;
+}
+
+void phil_loop(void)
+{
+    print("P>");
+
+}
