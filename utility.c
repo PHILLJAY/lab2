227430843139487db4f1643142147ab130f99525
PHILLJAY <philip.jasionowski@gmail.com>
2022-03-08 18:54:37 -0500

    stuff aint working lol

--- utility.c
+++ utility.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+
 
 //Commands yet to be created down below
 //TODO CD <directory - Change directory, if there is no <directory> augment present, print current directory
@@ -13,17 +13,52 @@ void clear(char* input){
 	return;
 }
 //TODO dir <directory> - List contents of <directory>
-void directory (char* input){
+void dir(char* input){
 	printf("directory");
 	return;
 }
 //TODO environ - list all envireonment strings
 
+void environ(char* input){
+	printf("directory");
+	return;
+}
+
 //TODO echo <comment> - display <comment> on the display followed by a new line
+
+void echo(char* input){
+	printf("directory");
+	return;
+}
+
+
 //TODO help - display user manual using the more filter
+
+void help(char* input){
+	printf("directory");
+	return;
+}
+
+
 //TODO pause - pause operation of the shell until 'enter' is pressed
+
+void pause(char* input){
+	printf("directory");
+	return;
+}
+
+
 //TODO quit - quit the shell
+
+void quit(char* input){
+	printf("directory");
+	return;
+}
+
 //TODO shell envireonment should contain shell=<pathname>/myshell where <pathname>/myshell is the full path for the shell executable (not hardwired, the one from where it was executed)
+
 //TODO all other input is interpreted as program invocation, the shell should fork and exec the programs as its own child processes.
+
 //TODO shell must be able to take its command line input from a file, if "fillshell batchfile" is ran it will assume the file contains a set of command lines for the shell to process, when end of file is reached the shell should exit
+
 //TODO shell must support background execution of programs, an "&" at the end of a command indicates that the shell should return the command line prompt immediately after launching that program
